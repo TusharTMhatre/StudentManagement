a8a90d28fc43cfbb2da5b0c6d379569bfeb3234f
Tushar Mhatre <ttmhatre@gmail.com>
2024-07-21 14:11:58 +0530

    Add project files.

---
+++ Controllers/registerationsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using StudentManagement.Models;
+
+namespace StudentManagement.Controllers
+{
+    public class registerationsController : Controller
+    {
+        private SMDBEntities db = new SMDBEntities();
+
+        // GET: registerations
+        public ActionResult Index()
+        {
+            var registerations = db.registerations.Include(r => r.batch).Include(r => r.course);
+            return View(registerations.ToList());
+        }
+
+        // GET: registerations/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            registeration registeration = db.registerations.Find(id);
+            if (registeration == null)
+            {
+                return HttpNotFound();
+            }
+            return View(registeration);
+        }
+
+        // GET: registerations/Create
+        public ActionResult Create()
+        {
+            ViewBag.batch_id = new SelectList(db.batches, "id", "batch1");
+            ViewBag.course_id = new SelectList(db.courses, "id", "coures");
+            return View();
+        }
+
+        // POST: registerations/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,firstname,lastname,nic,course_id,batch_id,telno")] registeration registeration)
+        {
+            if (ModelState.IsValid)
+            {
+                db.registerations.Add(registeration);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.batch_id = new SelectList(db.batches, "id", "batch1", registeration.batch_id);
+            ViewBag.course_id = new SelectList(db.courses, "id", "coures", registeration.course_id);
+            return View(registeration);
+        }
+
+        // GET: registerations/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            registeration registeration = db.registerations.Find(id);
+            if (registeration == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.batch_id = new SelectList(db.batches, "id", "batch1", registeration.batch_id);
+            ViewBag.course_id = new SelectList(db.courses, "id", "coures", registeration.course_id);
+            return View(registeration);
+        }
+
+        // POST: registerations/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,firstname,lastname,nic,course_id,batch_id,telno")] registeration registeration)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(registeration).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.batch_id = new SelectList(db.batches, "id", "batch1", registeration.batch_id);
+            ViewBag.course_id = new SelectList(db.courses, "id", "coures", registeration.course_id);
+            return View(registeration);
+        }
+
+        // GET: registerations/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            registeration registeration = db.registerations.Find(id);
+            if (registeration == null)
+            {
+                return HttpNotFound();
+            }
+            return View(registeration);
+        }
+
+        // POST: registerations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            registeration registeration = db.registerations.Find(id);
+            db.registerations.Remove(registeration);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
