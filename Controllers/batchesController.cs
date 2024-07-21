52e402e75c633b184b65c48fe1e68cde84915302
Tushar Mhatre <ttmhatre@gmail.com>
2024-07-21 14:11:58 +0530

    Add project files.

---
+++ Controllers/batchesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using StudentManagement.Models;
+
+namespace StudentManagement.Controllers
+{
+    public class batchesController : Controller
+    {
+        private SMDBEntities db = new SMDBEntities();
+
+        // GET: batches
+        public ActionResult Index()
+        {
+            return View(db.batches.ToList());
+        }
+
+        // GET: batches/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            batch batch = db.batches.Find(id);
+            if (batch == null)
+            {
+                return HttpNotFound();
+            }
+            return View(batch);
+        }
+
+        // GET: batches/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: batches/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,batch1,year")] batch batch)
+        {
+            if (ModelState.IsValid)
+            {
+                db.batches.Add(batch);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(batch);
+        }
+
+        // GET: batches/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            batch batch = db.batches.Find(id);
+            if (batch == null)
+            {
+                return HttpNotFound();
+            }
+            return View(batch);
+        }
+
+        // POST: batches/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,batch1,year")] batch batch)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(batch).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(batch);
+        }
+
+        // GET: batches/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            batch batch = db.batches.Find(id);
+            if (batch == null)
+            {
+                return HttpNotFound();
+            }
+            return View(batch);
+        }
+
+        // POST: batches/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            batch batch = db.batches.Find(id);
+            db.batches.Remove(batch);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
